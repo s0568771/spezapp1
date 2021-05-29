15844c74f9c43cee214db738f425ad59e2d611dd
Karsten <sass.karsten@googlemail.com>
2021-05-29 09:50:57 +0200

    deploy

--- ui/src/App.js
+++ ui/src/App.js
@@ -273,29 +273,29 @@ class App extends Component {
 
                           </div>
                           <div className="dish-favIcon">
-                            <IconButton onChange={e => this.handleChange(e)} color="secondary" aria-label="add an alarm" style={{"padding": 0}}>
-                              {/*{(food.fav === true) ? (*/}
+                            <IconButton onClick={e => this.handleChange(e)} color="secondary" aria-label="add an alarm" style={{"padding": 0}}>
+                              {(food.fav === true) ? (
                                 <div>
-                                  {/*<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>*/}
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                                   {console.log(food)}
                                   <Checkbox
                                     value={food.id}
-                                    defaultChecked={food.fav}
+                                    defaultchecked={true}
                                     // onChange={e => this.handleChange(e)}
                                     inputProps={{ 'aria-label': 'primary checkbox' }}
                                   />
                                 </div>
-                              {/*) : (*/}
-                              {/*  <div>*/}
-                              {/*    /!*<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>*!/*/}
-                              {/*    <Checkbox*/}
-                              {/*      value={food.id}*/}
-                              {/*      defaultChecked={false}*/}
-                              {/*      // onChange={e => this.handleChange(e)}*/}
-                              {/*      inputProps={{ 'aria-label': 'primary checkbox' }}*/}
-                              {/*    />*/}
-                              {/*  </div>*/}
-                              {/*)}*/}
+                              ) : (
+                                <div>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                                  <Checkbox
+                                    value={food.id}
+                                    defaultChecked={false}
+                                    // onChange={e => this.handleChange(e)}
+                                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                                  />
+                                </div>
+                              )}
                             </IconButton>
                           </div>
                         </div>
