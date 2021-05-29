c90ac4601a576bec08c3c1aa6755a03ad7ba7590
Karsten <sass.karsten@googlemail.com>
2021-05-29 12:34:38 +0200

    deploy

--- ui/src/App.js
+++ ui/src/App.js
@@ -157,7 +157,7 @@ class App extends Component {
           "id": fid,
           "name": fname
         }
-        console.log(body)
+        // console.log(body)
         axios.post('http://localhost:3000/api/insertFood', body, this.getOption());
       }
     })
@@ -266,7 +266,7 @@ class App extends Component {
                               {(food.fav === true) ? (
                                 <div>
                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
-                                  {console.log(food)}
+                                  {/*{console.log(food)}*/}
                                   <Checkbox
                                     value={food.id}
                                     defaultChecked={true}
